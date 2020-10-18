e94add2d82f855e4f6accbda0e02d05e1ecf7d0b
Sergio Aragonés <sergioat7@gmail.com>
2020-10-18 14:44:43 +0200

    Created BaseActivity and LandingActivity

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/activities/MainActivity.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/activities/MainActivity.kt
@@ -1,7 +1,13 @@
-package aragones.sergio.readercollection
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 18/10/2020
+ */
+
+package aragones.sergio.readercollection.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import aragones.sergio.readercollection.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
