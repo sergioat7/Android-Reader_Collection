7346c9eb2b200dc4f2013f0c61546a04e399686c
Sergio Aragonés <sergioat7@gmail.com>
2020-10-18 14:44:43 +0200

    Created BaseActivity and LandingActivity

---
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/activities/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 18/10/2020
+ */
+
+package aragones.sergio.readercollection.activities.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+}
