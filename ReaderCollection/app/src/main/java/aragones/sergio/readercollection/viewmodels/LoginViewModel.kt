d4ad6b9f1d302d25cc50e364e641f7c270b0b529
Sergio Aragonés <sergioat7@gmail.com>
2020-10-19 10:14:56 +0200

    Created Login view

---
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewmodels/LoginViewModel.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 19/10/2020
+ */
+
+package aragones.sergio.readercollection.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
