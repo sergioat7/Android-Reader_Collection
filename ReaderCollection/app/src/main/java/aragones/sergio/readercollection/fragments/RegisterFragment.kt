40458ea11ebf855c047c6a33dce3494609dd68e3
Sergio Aragonés <sergioat7@gmail.com>
2020-10-29 10:31:48 +0100

    Created Register view design

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/RegisterFragment.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/RegisterFragment.kt
@@ -9,19 +9,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.fragments.base.BaseFragment
 import aragones.sergio.readercollection.viewmodelfactories.RegisterViewModelFactory
 import aragones.sergio.readercollection.viewmodels.RegisterViewModel
+import kotlinx.android.synthetic.main.register_fragment.*
 
 class RegisterFragment: BaseFragment() {
+
+    //MARK: - Private properties
+
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btRegister: Button
+    private lateinit var viewModel: RegisterViewModel
+
+    //MARK: - Lifecycle methods
 
     companion object {
         fun newInstance() = RegisterFragment()
     }
-
-    private lateinit var viewModel: RegisterViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +51,10 @@ class RegisterFragment: BaseFragment() {
     private fun initializeUI() {
 
         val application = activity?.application ?: return
+        etUsername = edit_text_username
+        etPassword = edit_text_password
+        etConfirmPassword = edit_text_confirm_password
+        btRegister = button_register
         viewModel = ViewModelProvider(this, RegisterViewModelFactory(application)).get(RegisterViewModel::class.java)
         // TODO: Use the ViewModel
     }
