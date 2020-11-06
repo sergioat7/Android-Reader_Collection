d2a4b5156d6db1d1c43aa2d8a0ada0f32c279edc
Sergio Aragonés <sergioat7@gmail.com>
2020-11-06 20:58:05 +0100

    Created Profile view design

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/ProfileFragment.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/ProfileFragment.kt
@@ -6,21 +6,35 @@
 package aragones.sergio.readercollection.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.InputType
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.RadioButton
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.activities.LoginActivity
+import aragones.sergio.readercollection.extensions.afterTextChanged
+import aragones.sergio.readercollection.extensions.clearErrors
+import aragones.sergio.readercollection.extensions.setReadOnly
 import aragones.sergio.readercollection.fragments.base.BaseFragment
+import aragones.sergio.readercollection.utils.Constants
 import aragones.sergio.readercollection.viewmodelfactories.ProfileViewModelFactory
 import aragones.sergio.readercollection.viewmodels.ProfileViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment: BaseFragment() {
 
     //MARK: - Private properties
 
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var ibPassword: ImageButton
+    private lateinit var rbEnglish: RadioButton
+    private lateinit var rbSpanish: RadioButton
+    private lateinit var btSave: Button
     private lateinit var viewModel: ProfileViewModel
 
     //MARK: - Lifecycle methods
@@ -49,6 +63,10 @@ class ProfileFragment: BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when(item.itemId) {
+            R.id.action_delete -> {
+
+                return true
+            }
             R.id.action_logout -> {
 
                 viewModel.logout()
@@ -63,23 +81,42 @@ class ProfileFragment: BaseFragment() {
     private fun initializeUI() {
 
         val application = activity?.application ?: return
+        etUsername = edit_text_username
+        etPassword = edit_text_password
+        ibPassword = image_button_password
+        rbEnglish = radio_button_en
+        rbSpanish = radio_button_es
+        btSave = button_save
         viewModel = ViewModelProvider(this, ProfileViewModelFactory(application)).get(ProfileViewModel::class.java)
         setupBindings()
 
-        val language = viewModel.language
+        etUsername.setText(viewModel.profileUserData.value?.username)
+        etPassword.setText(viewModel.profileUserData.value?.password)
+        etUsername.setReadOnly(true, InputType.TYPE_NULL, 0)
 
-        val userData = viewModel.profileUserData.value
-        val username = userData?.username
-        val password = userData?.password
+        etPassword.afterTextChanged {
+            viewModel.profileDataChanged(it)
+        }
 
-        print(username)
-        print(password)
+        ibPassword.setOnClickListener {
+            Constants.showOrHidePassword(etPassword, ibPassword, Constants.isDarkMode(context))
+        }
+
     }
 
     private fun setupBindings() {
 
         viewModel.profileUserData.observe(viewLifecycleOwner, Observer {
             viewModel.login(it.username, it.password)
+        })
+
+        viewModel.profileForm.observe(viewLifecycleOwner, Observer {
+
+            etPassword.clearErrors()
+            btSave.isEnabled = it == null
+
+            val passwordError = it ?: return@Observer
+            etPassword.error = getString(passwordError)
         })
 
         viewModel.profileRedirection.observe(viewLifecycleOwner, Observer { redirect ->
