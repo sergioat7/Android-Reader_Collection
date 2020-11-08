1b04f473bf40e1dd7c033ad3533dee356bb5f964
Sergio Aragonés <sergioat7@gmail.com>
2020-11-08 19:40:26 +0100

    Created search button

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/SearchFragment.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/SearchFragment.kt
@@ -5,13 +5,17 @@
 
 package aragones.sergio.readercollection.fragments
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.fragments.base.BaseFragment
+import aragones.sergio.readercollection.utils.Constants
 import aragones.sergio.readercollection.viewmodelfactories.SearchViewModelFactory
 import aragones.sergio.readercollection.viewmodels.SearchViewModel
 
@@ -28,12 +32,23 @@ class SearchFragment: BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         initializeUI()
+    }
+
+    //MARK: - Public methods
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        menu.clear()
+        inflater.inflate(R.menu.search_toolbar_menu, menu)
+        setupSearchView(menu)
     }
 
     //MARK: - Private methods
@@ -66,5 +81,53 @@ class SearchFragment: BaseFragment() {
                 hideLoading()
             }
         })
+    }
+
+    private fun setupSearchView(menu: Menu) {
+
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager?
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        if (searchManager != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+            searchView.isIconified = false
+            searchView.isIconifiedByDefault = false
+            searchView.queryHint = resources.getString(R.string.search_books)
+            searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+
+                    searchBooks(query)
+                    return true
+                }
+            })
+        }
+        val searchPlateId = searchView.context.resources.getIdentifier("android:id/search_plate", null, null)
+        val searchPlate = searchView.findViewById<View>(searchPlateId)
+        if (searchPlate != null) {
+
+            val searchTextId = searchPlate.context.resources.getIdentifier(
+                "android:id/search_src_text",
+                null,
+                null
+            )
+            val searchText = searchPlate.findViewById<TextView>(searchTextId)
+            if (searchText != null) {
+
+                searchText.setTextColor(ContextCompat.getColor(requireActivity(), R.color.colorSecondary))
+                searchText.setHintTextColor(ContextCompat.getColor(requireActivity(),R.color.colorSecondary))
+            }
+        }
+    }
+
+    private fun searchBooks(query: String) {
+
+        viewModel.setSearch(query)
+        viewModel.reloadData()
+        viewModel.searchBooks()
+        Constants.hideSoftKeyboard(requireActivity())
     }
 }
