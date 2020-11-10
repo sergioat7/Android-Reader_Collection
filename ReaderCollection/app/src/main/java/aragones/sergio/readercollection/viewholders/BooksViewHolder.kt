6038bb5fbcc83c9b1625a33675036e302e6243e5
Sergio Aragonés <sergioat7@gmail.com>
2020-11-10 11:31:29 +0100

    Set default image when book has no thumbnail

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewholders/BooksViewHolder.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewholders/BooksViewHolder.kt
@@ -8,10 +8,12 @@ package aragones.sergio.readercollection.viewholders
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.models.responses.BookResponse
+import aragones.sergio.readercollection.utils.Constants
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.book_item.view.*
@@ -20,16 +22,20 @@ class BooksViewHolder(
 itemView: View
 ) : RecyclerView.ViewHolder(itemView) {
 
+    //MARK: - Public methods
+
     fun fillData(book: BookResponse, context: Context) {
 
+        val image = book.thumbnail?.replace("http", "https") ?: "-"
+        val errorImage = if (Constants.isDarkMode(context)) R.drawable.default_book_cover_dark else R.drawable.default_book_cover_light
         val loading = itemView.progress_bar_loading
         loading.visibility = View.VISIBLE
         Picasso
             .get()
-            .load(book.thumbnail)
+            .load(image)
             .fit()
             .centerCrop()
-//            .error(R.drawable.no_image)
+            .error(errorImage)
             .into(itemView.image_view_book, object: Callback {
 
                 override fun onSuccess() {
@@ -48,8 +54,7 @@ itemView: View
         book.categories?.let { categories ->
             for (category in categories) {
 
-                val tv = TextView(context)
-                tv.text = category
+                val tv = getRoundedTextView(category, context)
                 llCategories.addView(tv)
 
                 val view = View(context)
@@ -60,5 +65,18 @@ itemView: View
                 llCategories.addView(view)
             }
         }
+    }
+
+    //MARK: - Private methods
+
+    private fun getRoundedTextView(text: String, context: Context): TextView {
+
+        val tv = TextView(context, null, R.style.RoundedTextView, R.style.RoundedTextView)
+        tv.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        tv.text = text
+        return tv
     }
 }
