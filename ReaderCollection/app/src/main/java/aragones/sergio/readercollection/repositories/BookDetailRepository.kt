b2cbbc153774841a21070c4ee4c77fafebca21bd
Sergio Aragonés <sergioat7@gmail.com>
2020-11-11 14:28:34 +0100

    Inject BookAPIClient to BookDetailRepository by multi module component

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/repositories/BookDetailRepository.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/repositories/BookDetailRepository.kt
@@ -5,18 +5,25 @@
 
 package aragones.sergio.readercollection.repositories
 
+import aragones.sergio.readercollection.models.responses.BookResponse
 import aragones.sergio.readercollection.models.responses.GoogleBookResponse
+import aragones.sergio.readercollection.network.apiclient.BookAPIClient
 import aragones.sergio.readercollection.network.apiclient.GoogleAPIClient
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class BookDetailRepository @Inject constructor(
+    private val bookAPIClient: BookAPIClient,
     private val googleAPIClient: GoogleAPIClient
 ) {
 
     //MARK: - Public methods
 
-    fun getBook(volumeId: String): Single<GoogleBookResponse> {
+    fun getBook(googleId: String): Single<BookResponse> {
+        return bookAPIClient.getBookObserver(googleId)
+    }
+
+    fun getGoogleBook(volumeId: String): Single<GoogleBookResponse> {
         return googleAPIClient.getGoogleBookObserver(volumeId)
     }
 }
