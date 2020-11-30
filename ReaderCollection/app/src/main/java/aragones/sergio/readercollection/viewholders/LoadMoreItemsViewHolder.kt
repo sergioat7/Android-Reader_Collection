347f43b39d1c3ee8c1dda9369be24f6a258888a1
Sergio Aragonés <sergioat7@gmail.com>
2020-11-30 14:13:18 +0100

    Created cell to load more google books on user's demand

---
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewholders/LoadMoreItemsViewHolder.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 30/11/2020
+ */
+
+package aragones.sergio.readercollection.viewholders
+
+import android.view.View
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import aragones.sergio.readercollection.R
+import aragones.sergio.readercollection.adapters.OnItemClickListener
+
+class LoadMoreItemsViewHolder(
+    itemView: View
+) : RecyclerView.ViewHolder(itemView) {
+
+    //MARK: - Public methods
+
+    fun setItem(onItemClickListener: OnItemClickListener) {
+
+        val btLoadMoreItems = itemView.findViewById<Button>(R.id.button_load_more_items)
+        btLoadMoreItems.setOnClickListener {
+            onItemClickListener.onLoadMoreItemsClick()
+        }
+    }
+}
