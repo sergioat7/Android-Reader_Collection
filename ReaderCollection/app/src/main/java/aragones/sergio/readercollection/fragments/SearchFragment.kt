c1a6c48748779b1a8202d7557fd65ed3d834d3be
Sergio Aragonés <sergioat7@gmail.com>
2021-01-16 00:44:54 +0100

    Code refactoring

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/SearchFragment.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/SearchFragment.kt
@@ -5,16 +5,10 @@
 
 package aragones.sergio.readercollection.fragments
 
-import android.app.SearchManager
-import android.content.Context
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.*
 import android.widget.SearchView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -194,20 +188,12 @@ class SearchFragment: BaseFragment(), OnItemClickListener {
     }
 
     private fun setupSearchView(menu: Menu) {
-        
+
         val menuItem = menu.findItem(R.id.action_search)
-        val searchView = menuItem.actionView as SearchView
+        this.searchView = menuItem.actionView as SearchView
+        this.searchView?.let { searchView ->
 
-        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager?
-        if (searchManager != null) {
-
-            searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
-            searchView.isIconified = false
-            searchView.isIconifiedByDefault = false
             searchView.queryHint = resources.getString(R.string.search_books)
-            if (viewModel.query.isNotBlank()) {
-                searchView.setQuery(viewModel.query, false)
-            }
             searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
 
                 override fun onQueryTextChange(newText: String): Boolean {
@@ -222,43 +208,7 @@ class SearchFragment: BaseFragment(), OnItemClickListener {
                 }
             })
         }
-
-        val color = ContextCompat.getColor(requireActivity(), R.color.textTertiary)
-
-        val searchIconId = searchView.context.resources.getIdentifier(
-            "android:id/search_mag_icon",
-            null,
-            null
-        )
-        searchView.findViewById<AppCompatImageView>(searchIconId)?.imageTintList = ColorStateList.valueOf(color)
-
-        val searchPlateId = searchView.context.resources.getIdentifier(
-            "android:id/search_plate",
-            null,
-            null
-        )
-        val searchPlate = searchView.findViewById<View>(searchPlateId)
-        if (searchPlate != null) {
-
-            val searchTextId = searchPlate.context.resources.getIdentifier(
-                "android:id/search_src_text",
-                null,
-                null
-            )
-            val searchText = searchPlate.findViewById<TextView>(searchTextId)
-            if (searchText != null) {
-
-                searchText.setTextColor(color)
-                searchText.setHintTextColor(color)
-            }
-
-            val searchCloseId = searchPlate.context.resources.getIdentifier(
-                "android:id/search_close_btn",
-                null,
-                null
-            )
-            searchPlate.findViewById<AppCompatImageView>(searchCloseId)?.imageTintList = ColorStateList.valueOf(color)
-        }
+        this.setupSearchView(viewModel.query)
     }
 
     private fun searchBooks(query: String) {
