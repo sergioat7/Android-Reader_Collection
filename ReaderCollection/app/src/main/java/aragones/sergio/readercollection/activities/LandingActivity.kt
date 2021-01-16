d303444948b58ca1c7155fedd7bdfd334d967faa
Sergio Aragonés <sergioat7@gmail.com>
2021-01-16 19:12:54 +0100

    Created LandingViewModel

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/activities/LandingActivity.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/activities/LandingActivity.kt
@@ -7,71 +7,49 @@ package aragones.sergio.readercollection.activities
 
 import android.content.Intent
 import android.os.Bundle
-import aragones.sergio.readercollection.BuildConfig
+import androidx.lifecycle.ViewModelProvider
 import aragones.sergio.readercollection.activities.base.BaseActivity
-import aragones.sergio.readercollection.injection.ReaderCollectionApplication
-import aragones.sergio.readercollection.utils.SharedPreferencesHandler
+import aragones.sergio.readercollection.viewmodelfactories.LandingViewModelFactory
+import aragones.sergio.readercollection.viewmodels.LandingViewModel
 import java.util.*
-import javax.inject.Inject
-import kotlin.math.max
 
 class LandingActivity: BaseActivity() {
 
-    //MARK: - Public properties
+    //MARK: - Private properties
 
-    @Inject
-    lateinit var sharedPreferencesHandler: SharedPreferencesHandler
+    private lateinit var viewModel: LandingViewModel
 
     //MARK: - Lifecycle methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        (application as ReaderCollectionApplication).sharedPreferencesComponent.inject(this)
-
-        checkVersion()
-
-        configLanguage()
-
-        val initTime = System.currentTimeMillis() / 1000
-        val cls: Class<*> = if (sharedPreferencesHandler.isLoggedIn()) {
-            MainActivity::class.java
-        } else {
-            LoginActivity::class.java
-        }
-        val finalTime = System.currentTimeMillis() / 1000
-        val taskTime = finalTime - initTime
-        val time = max(0, 1000 - taskTime)
-
-        try {
-            Thread.sleep(time)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
-
-        val intent = Intent(this, cls)
-        startActivity(intent)
+        initializeUI()
     }
 
     //MARK: - Private methods
 
+    private fun initializeUI() {
+        viewModel = ViewModelProvider(this, LandingViewModelFactory(application)).get(LandingViewModel::class.java)
+        setupBindings()
+
+        configLanguage()
+        viewModel.checkVersion()
+    }
+
+    private fun setupBindings() {
+
+        viewModel.landingClassToStart.observe(this, {
+
+            val intent = Intent(this, it)
+            startActivity(intent)
+        })
+    }
+
     private fun configLanguage() {
 
-        val language: String = sharedPreferencesHandler.getLanguage()
+        val language = viewModel.language
         val conf = resources.configuration
         conf.setLocale(Locale(language))
         resources.updateConfiguration(conf, resources.displayMetrics)
-    }
-
-    private fun checkVersion() {
-
-        val currentVersion = sharedPreferencesHandler.getVersion()
-        val newVersion = BuildConfig.VERSION_CODE
-        if (newVersion > currentVersion) {
-
-            sharedPreferencesHandler.setVersion(newVersion)
-            sharedPreferencesHandler.removePassword()
-            sharedPreferencesHandler.removeCredentials()
-        }
     }
 }
