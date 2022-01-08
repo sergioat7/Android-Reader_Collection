d0dd4c9795d4e2b9459077df5f2301e9b39ccdd7
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-08 19:15:51 +0100

    Created binding adapters for spinner and rating bar

---
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/extensions/BindingAdapters.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 7/1/2022
+ */
+
+package aragones.sergio.readercollection.extensions
+
+import android.content.res.ColorStateList
+import android.widget.Spinner
+import androidx.databinding.BindingAdapter
+import me.zhanghai.android.materialratingbar.MaterialRatingBar
+
+@BindingAdapter(value = ["isEnabled", "backgroundTint"], requireAll = false)
+fun setEnabled(spinner: Spinner, isEnabled: Boolean?, backgroundTint: Int?) {
+    spinner.isEnabled = isEnabled == true
+    backgroundTint?.let {
+        spinner.backgroundTintList = ColorStateList.valueOf(backgroundTint)
+    }
+}
+
+@BindingAdapter("rating")
+fun setRating(ratingBar: MaterialRatingBar, rating: Double?) {
+    ratingBar.rating = rating?.toInt()?.toFloat() ?: 0F
+}
