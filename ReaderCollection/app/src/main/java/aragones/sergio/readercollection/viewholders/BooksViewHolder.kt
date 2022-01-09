a8df714078ea65e293edd1cfd50789d9842ba9b3
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-09 14:32:55 +0100

    Created data binding for images

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewholders/BooksViewHolder.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewholders/BooksViewHolder.kt
@@ -5,19 +5,13 @@
 
 package aragones.sergio.readercollection.viewholders
 
-import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.adapters.OnItemClickListener
 import aragones.sergio.readercollection.databinding.ItemBookBinding
 import aragones.sergio.readercollection.databinding.ItemGoogleBookBinding
 import aragones.sergio.readercollection.databinding.ItemReadingBookBinding
-import aragones.sergio.readercollection.extensions.getRoundImageView
 import aragones.sergio.readercollection.models.responses.BookResponse
-import aragones.sergio.readercollection.utils.Constants
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
 import kotlin.math.ceil
 
 class BooksViewHolder(private val binding: ViewDataBinding) :
@@ -31,83 +25,17 @@ class BooksViewHolder(private val binding: ViewDataBinding) :
                 is ItemReadingBookBinding -> {
                     this.book = book
                     this.onItemClickListener = onItemClickListener
-
-                    val image = book.thumbnail?.replace("http", "https") ?: "-"
-                    progressBarReadingImageLoading.visibility = View.VISIBLE
-                    Picasso
-                        .get()
-                        .load(image)
-                        .fit()
-                        .centerCrop()
-                        .error(R.drawable.ic_default_book_cover)//TODO: change cover
-                        .into(imageViewReadingBook, object : Callback {
-
-                            override fun onSuccess() {
-                                imageViewReadingBook.apply {
-                                    this.setImageDrawable(this.getRoundImageView(Constants.IMAGE_CORNER))
-                                }
-                                progressBarReadingImageLoading.visibility = View.GONE
-                            }
-
-                            override fun onError(e: Exception) {
-                                progressBarReadingImageLoading.visibility = View.GONE
-                            }
-                        })
                 }
 
                 is ItemGoogleBookBinding -> {
                     this.book = book
                     this.onItemClickListener = onItemClickListener
-
-                    val image = book.thumbnail?.replace("http", "https") ?: "-"
-                    progressBarGoogleImageLoading.visibility = View.VISIBLE
-                    Picasso
-                        .get()
-                        .load(image)
-                        .fit()
-                        .centerCrop()
-                        .error(R.drawable.ic_default_book_cover)
-                        .into(imageViewGoogleBook, object : Callback {
-
-                            override fun onSuccess() {
-                                imageViewGoogleBook.apply {
-                                    this.setImageDrawable(this.getRoundImageView(Constants.IMAGE_CORNER))
-                                }
-                                progressBarGoogleImageLoading.visibility = View.GONE
-                            }
-
-                            override fun onError(e: Exception) {
-                                progressBarGoogleImageLoading.visibility = View.GONE
-                            }
-                        })
                     textViewGoogleBookRating.text = ceil(book.averageRating).toInt().toString()
                 }
 
                 is ItemBookBinding -> {
                     this.book = book
                     this.onItemClickListener = onItemClickListener
-
-                    val image = book.thumbnail?.replace("http", "https") ?: "-"
-                    progressBarImageLoading.visibility = View.VISIBLE
-                    Picasso
-                        .get()
-                        .load(image)
-                        .fit()
-                        .centerCrop()
-                        .error(R.drawable.ic_default_book_cover)
-                        .into(imageViewBook, object : Callback {
-
-                            override fun onSuccess() {
-                                imageViewBook.apply {
-                                    this.setImageDrawable(this.getRoundImageView(Constants.IMAGE_CORNER))
-                                }
-                                progressBarImageLoading.visibility = View.GONE
-                            }
-
-                            override fun onError(e: Exception) {
-                                progressBarImageLoading.visibility = View.GONE
-                            }
-                        })
                 }
                 else -> Unit
             }
