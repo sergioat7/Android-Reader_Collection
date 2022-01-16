59e1bc4d902243e4675fa5b46cfee211cec43ecb
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-16 18:22:30 +0100

    Removed format & state apis

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/models/responses/FormatResponse.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/models/responses/FormatResponse.kt
@@ -8,13 +8,10 @@ package aragones.sergio.readercollection.models.responses
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import aragones.sergio.readercollection.base.BaseModel
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "Format")
 data class FormatResponse(
     @PrimaryKey
-    @SerializedName("id")
     override val id: String,
-    @SerializedName("name")
     val name: String
-): BaseModel<String>
+) : BaseModel<String>
