edadb6b319c7eba60de7d9f61c92fde2cd1ea290
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-16 18:40:09 +0100

    Removed format & state tables from database

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/models/FormatResponse.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/models/FormatResponse.kt
@@ -1,17 +1,13 @@
 /*
- * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
- * Created by Sergio Aragonés on 19/10/2020
+ * Copyright (c) 2022 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 16/1/2022
  */
 
 package aragones.sergio.readercollection.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import aragones.sergio.readercollection.base.BaseModel
 
-@Entity(tableName = "Format")
 data class FormatResponse(
-    @PrimaryKey
     override val id: String,
     val name: String
 ) : BaseModel<String>
