c073723cc11ca5b568f0655555bafcfb94afcd83
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-17 22:18:30 +0100

    Created Book list view

---
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/BookListFragment.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 17/1/2022
+ */
+
+package aragones.sergio.readercollection.fragments
+
+import android.os.Bundle
+import android.view.View
+import aragones.sergio.readercollection.base.BindingFragment
+import aragones.sergio.readercollection.databinding.FragmentBookListBinding
+import aragones.sergio.readercollection.extensions.isDarkMode
+import aragones.sergio.readercollection.utils.StatusBarStyle
+
+class BookListFragment : BindingFragment<FragmentBookListBinding>() {
+
+    //region Protected properties
+    override val hasOptionsMenu = true
+    override val statusBarStyle = StatusBarStyle.PRIMARY
+    //endregion
+
+    //region Lifecycle methods
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbar = binding.toolbar
+        initializeUi()
+    }
+
+    override fun initializeUi() {
+        super.initializeUi()
+
+        binding.isDarkMode = context?.isDarkMode()
+    }
+    //endregion
+}
