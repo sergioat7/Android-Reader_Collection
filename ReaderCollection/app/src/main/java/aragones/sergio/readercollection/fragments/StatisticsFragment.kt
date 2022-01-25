2e792011e900592c731d6332c6154dafeb7809c2
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-25 16:07:33 +0100

    Created Statistics view model

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/StatisticsFragment.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/fragments/StatisticsFragment.kt
@@ -7,9 +7,12 @@ package aragones.sergio.readercollection.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import aragones.sergio.readercollection.base.BindingFragment
 import aragones.sergio.readercollection.databinding.FragmentStatisticsBinding
 import aragones.sergio.readercollection.utils.StatusBarStyle
+import aragones.sergio.readercollection.viewmodelfactories.StatisticsViewModelFactory
+import aragones.sergio.readercollection.viewmodels.StatisticsViewModel
 
 class StatisticsFragment : BindingFragment<FragmentStatisticsBinding>() {
 
@@ -18,12 +21,34 @@ class StatisticsFragment : BindingFragment<FragmentStatisticsBinding>() {
     override val statusBarStyle = StatusBarStyle.PRIMARY
     //endregion
 
+    //region Private properties
+    private lateinit var viewModel: StatisticsViewModel
+    //endregion
+
     //region Lifecycle methods
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         toolbar = binding.toolbar
         initializeUi()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (this::viewModel.isInitialized) viewModel.onDestroy()
+    }
+    //endregion
+
+    //region Protected methods
+    override fun initializeUi() {
+        super.initializeUi()
+
+        val application = activity?.application ?: return
+        viewModel = ViewModelProvider(
+            this,
+            StatisticsViewModelFactory(application)
+        )[StatisticsViewModel::class.java]
     }
     //endregion
 }
