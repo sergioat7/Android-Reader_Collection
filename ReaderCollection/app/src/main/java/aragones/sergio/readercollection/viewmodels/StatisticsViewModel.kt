afaaff1b1291bd778a228f44eb996f3f6b34388f
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-01-25 17:31:23 +0100

    Created format stats

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewmodels/StatisticsViewModel.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewmodels/StatisticsViewModel.kt
@@ -9,9 +9,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.base.BaseViewModel
+import aragones.sergio.readercollection.models.responses.BookResponse
 import aragones.sergio.readercollection.models.responses.ErrorResponse
 import aragones.sergio.readercollection.repositories.BooksRepository
+import aragones.sergio.readercollection.utils.Constants
 import aragones.sergio.readercollection.utils.State
+import com.github.mikephil.charting.data.PieEntry
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import javax.inject.Inject
@@ -23,11 +26,13 @@ class StatisticsViewModel @Inject constructor(
     //region Private properties
     private val _booksLoading = MutableLiveData<Boolean>()
     private val _booksError = MutableLiveData<ErrorResponse>()
+    private val _formatStats = MutableLiveData<List<PieEntry>>()
     //endregion
 
     //region Public properties
     val booksLoading: LiveData<Boolean> = _booksLoading
     val booksError: LiveData<ErrorResponse> = _booksError
+    val formatStats: LiveData<List<PieEntry>> = _formatStats
     //endregion
 
     //region Lifecycle methods
@@ -56,6 +61,7 @@ class StatisticsViewModel @Inject constructor(
                     noBooksError()
                 } else {
                     //TODO: handle results
+                    createFormatStats(it)
                     _booksLoading.value = false
                 }
             },
@@ -72,6 +78,20 @@ class StatisticsViewModel @Inject constructor(
         _booksLoading.value = false
         _booksError.value = ErrorResponse("", R.string.error_database)
         onDestroy()
+    }
+
+    private fun createFormatStats(books: List<BookResponse>) {
+
+        val entries = mutableListOf<PieEntry>()
+        for (entry in books.groupBy { it.format }.entries) {
+            entries.add(
+                PieEntry(
+                    entry.value.size.toFloat(),
+                    Constants.FORMATS.first { it.id == entry.key }.name
+                )
+            )
+        }
+        _formatStats.value = entries
     }
     //endregion
 }
