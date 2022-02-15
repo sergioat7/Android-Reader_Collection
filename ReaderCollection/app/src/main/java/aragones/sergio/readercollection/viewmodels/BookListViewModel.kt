38cd2b2b2eacfd98da477367253223067856db66
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-02-15 10:00:41 +0100

    Sorted books on sort button click

--- ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewmodels/BookListViewModel.kt
+++ ReaderCollection/app/src/main/java/aragones/sergio/readercollection/viewmodels/BookListViewModel.kt
@@ -5,16 +5,23 @@
 
 package aragones.sergio.readercollection.viewmodels
 
+import android.content.Context
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.NumberPicker
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import aragones.sergio.readercollection.R
 import aragones.sergio.readercollection.base.BaseViewModel
 import aragones.sergio.readercollection.extensions.getMonthNumber
+import aragones.sergio.readercollection.extensions.getPickerParams
 import aragones.sergio.readercollection.extensions.getYear
+import aragones.sergio.readercollection.extensions.setup
 import aragones.sergio.readercollection.models.responses.BookResponse
 import aragones.sergio.readercollection.models.responses.ErrorResponse
 import aragones.sergio.readercollection.repositories.BooksRepository
 import aragones.sergio.readercollection.utils.ScrollPosition
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import javax.inject.Inject
@@ -122,6 +129,49 @@ class BookListViewModel @Inject constructor(
 
     fun setPosition(newPosition: ScrollPosition) {
         _scrollPosition.value = newPosition
+    }
+
+    fun sort(context: Context) {
+
+        val sortingKeys = context.resources.getStringArray(R.array.sorting_param_keys)
+        val sortingValues = context.resources.getStringArray(R.array.sorting_param_values)
+
+        val dialogView = LinearLayout(context)
+        dialogView.orientation = LinearLayout.HORIZONTAL
+
+        val sortKeysPicker = NumberPicker(context)
+        sortKeysPicker.setup(sortingValues)
+        sortParam?.let {
+            sortKeysPicker.value = sortingKeys.indexOf(it)
+        }
+
+        val sortOrdersPicker = NumberPicker(context)
+        sortOrdersPicker.setup(context.resources.getStringArray(R.array.sorting_order_values))
+        sortOrdersPicker.value = if (isSortDescending) 1 else 0
+
+        val params = LinearLayout.LayoutParams(50, 50)
+        params.gravity = Gravity.CENTER
+
+        dialogView.layoutParams = params
+        dialogView.addView(sortKeysPicker, getPickerParams())
+        dialogView.addView(sortOrdersPicker, getPickerParams())
+
+        MaterialAlertDialogBuilder(context)
+            .setTitle(context.resources.getString(R.string.order_by))
+            .setView(dialogView)
+            .setCancelable(false)
+            .setPositiveButton(context.resources.getString(R.string.accept)) { dialog, _ ->
+
+                val sort = sortingKeys[sortKeysPicker.value]
+                sortParam = sort.ifBlank { null }
+                isSortDescending = sortOrdersPicker.value == 1
+                fetchBooks()
+                dialog.dismiss()
+            }
+            .setNegativeButton(context.resources.getString(R.string.cancel)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
     //endregion
 
