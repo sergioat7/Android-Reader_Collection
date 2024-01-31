1f6858f7677c370d6b19cb2daedfada7a3b8b081
Sergio Aragonés <sergioat7@gmail.com>
2024-01-31 20:58:10 +0100

    Migrate plugins to Version Catalogs

--- ReaderCollection/build.gradle.kts
+++ ReaderCollection/build.gradle.kts
@@ -3,14 +3,13 @@ buildscript {
     dependencies {
         classpath("com.google.firebase:firebase-crashlytics-gradle:${libs.versions.crashlytics.get()}")//
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${libs.versions.navigation.get()}")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:${libs.versions.hilt.get()}")//
     }
 }
 
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt)
 }
 
 tasks.register("clean", Delete::class) {
