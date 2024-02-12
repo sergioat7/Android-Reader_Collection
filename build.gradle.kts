0f2b0d15b66e10b0fcc24d17942de52c1c429a5b
Sergio Aragonés <sergioat7@gmail.com>
2024-02-12 20:33:27 +0100

    Code refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ buildscript {
     }
 }
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
