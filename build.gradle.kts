a7c53f16d72b3a37160d189db07fe0a1b14f11a3
Sergio Aragonés <sergioat7@gmail.com>
2024-02-22 19:50:35 +0100

    Create core module

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 tasks.register("clean", Delete::class) {
