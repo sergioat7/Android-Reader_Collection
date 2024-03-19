6aa5bf054962e3664adb4a2ad91e66a209c901ee
Sergio Aragonés <sergioat7@gmail.com>
2024-03-19 17:11:43 +0100

    Create textfield dialog in Compose

--- app/src/main/java/aragones/sergio/readercollection/ui/Dialogs.kt
+++ app/src/main/java/aragones/sergio/readercollection/ui/Dialogs.kt
@@ -6,15 +6,23 @@
 package aragones.sergio.readercollection.ui
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -119,6 +127,95 @@ fun InformationAlertDialog(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun TextFieldAlertDialogPreview() {
+    TextFieldAlertDialog(true, R.string.enter_valid_url, {}, {})
+}
+
+@Composable
+fun TextFieldAlertDialog(
+    show: Boolean,
+    @StringRes titleTextId: Int,
+    onCancel: () -> Unit,
+    onAccept: (String) -> Unit
+) {
+    if (show) {
+
+        var text by rememberSaveable { mutableStateOf("") }
+        val padding12 = dimensionResource(id = R.dimen.padding_12dp).value
+        val padding24 = dimensionResource(id = R.dimen.padding_24dp).value
+        val colorPrimary = colorResource(id = R.color.colorPrimary)
+        val colorPrimaryLight = colorResource(id = R.color.colorPrimaryLight)
+        val colorSecondary = colorResource(id = R.color.colorSecondary)
+        val textSecondary = colorResource(id = R.color.textSecondary)
+
+        Dialog(
+            onDismissRequest = { onCancel() },
+            properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
+        ) {
+            Surface(
+                color = colorSecondary,
+                shape = RoundedCornerShape(10.dp)
+            ) {
+                Column(Modifier.fillMaxWidth()) {
+                    TextTitleAlertDialog(titleTextId)
+                    OutlinedTextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(start = padding24.dp, end = padding24.dp)
+                            .border(
+                                BorderStroke(1.dp, colorResource(id = R.color.colorPrimary)),
+                                shape = RoundedCornerShape(10.dp)
+                            ),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = colorPrimary,
+                            unfocusedBorderColor = colorPrimaryLight,
+                            textColor = textSecondary
+                        ),
+                        shape = RoundedCornerShape(10.dp),
+                        singleLine = true
+                    )
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .padding(end = padding12.dp)
+                    ) {
+                        TextButtonAlertDialog(textId = R.string.cancel) {
+                            onCancel()
+                        }
+                        TextButtonAlertDialog(textId = R.string.accept) {
+                            onAccept(text)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextTitleAlertDialog(@StringRes textId: Int) {
+
+    val padding8 = dimensionResource(id = R.dimen.padding_8dp).value
+    val padding24 = dimensionResource(id = R.dimen.padding_24dp).value
+    Text(
+        modifier = Modifier.padding(
+            top = padding24.dp,
+            start = padding24.dp,
+            end = padding24.dp,
+            bottom = padding8.dp
+        ),
+        text = stringResource(id = textId),
+        color = colorResource(id = R.color.colorPrimary),
+        fontSize = dimensionResource(id = R.dimen.text_size_18sp).value.sp,
+        fontFamily = robotoSerifFamily,
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Composable
