ce4006ed94241343586c2b59f59cfb39e4e5c1b9
Sergio Aragonés <sergioat7@gmail.com>
2024-03-23 20:57:15 +0100

    Load compose view

--- app/src/main/java/aragones/sergio/readercollection/ui/login/LoginFragment.kt
+++ app/src/main/java/aragones/sergio/readercollection/ui/login/LoginFragment.kt
@@ -9,9 +9,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import aragones.sergio.readercollection.databinding.FragmentLoginBinding
-import aragones.sergio.readercollection.extensions.doAfterTextChanged
-import aragones.sergio.readercollection.extensions.getValue
-import aragones.sergio.readercollection.extensions.setError
 import aragones.sergio.readercollection.ui.MainActivity
 import aragones.sergio.readercollection.ui.base.BindingFragment
 import aragones.sergio.readercollection.ui.register.RegisterActivity
@@ -33,7 +30,11 @@ class LoginFragment : BindingFragment<FragmentLoginBinding>() {
     //region Lifecycle methods
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializeUi()
+
+        binding.composeView.setContent {
+            LoginScreen()
+        }
+        setupBindings()
     }
 
     override fun onDestroy() {
@@ -46,56 +47,10 @@ class LoginFragment : BindingFragment<FragmentLoginBinding>() {
     fun goToRegister() {
         launchActivity(RegisterActivity::class.java)
     }
-
-    fun login() {
-
-        binding.textInputLayoutUsername.textInputEditText.clearFocus()
-        binding.textInputLayoutPassword.textInputEditText.clearFocus()
-        viewModel.login(
-            binding.textInputLayoutUsername.getValue(),
-            binding.textInputLayoutPassword.getValue()
-        )
-    }
-    //endregion
-
-    //region Protected methods
-    override fun initializeUi() {
-
-        setupBindings()
-
-        binding.fragment = this
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        binding.textInputLayoutUsername.doAfterTextChanged {
-            loginDataChanged()
-        }
-        binding.textInputLayoutPassword.doAfterTextChanged {
-            loginDataChanged()
-        }
-    }
     //endregion
 
     //region Private methods
     private fun setupBindings() {
-
-        viewModel.loginFormState.observe(viewLifecycleOwner) {
-
-            binding.textInputLayoutUsername.setError("")
-            binding.textInputLayoutPassword.setError("")
-            val loginState = it ?: return@observe
-
-            if (loginState.usernameError != null) {
-                binding.textInputLayoutUsername.setError(getString(loginState.usernameError ?: 0))
-            }
-            if (loginState.passwordError != null) {
-                binding.textInputLayoutPassword.setError(getString(loginState.passwordError ?: 0))
-            }
-        }
 
         viewModel.loginLoading.observe(viewLifecycleOwner) { isLoading ->
 
@@ -116,14 +71,6 @@ class LoginFragment : BindingFragment<FragmentLoginBinding>() {
                 manageError(error)
             }
         }
-    }
-
-    private fun loginDataChanged() {
-
-        viewModel.loginDataChanged(
-            binding.textInputLayoutUsername.getValue(),
-            binding.textInputLayoutPassword.getValue()
-        )
     }
     //endregion
 }
