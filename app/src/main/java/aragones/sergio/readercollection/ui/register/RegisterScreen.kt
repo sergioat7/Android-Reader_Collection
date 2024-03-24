a61daca8cb13115ab0702c23aeeb4d580d20aac9
Sergio Aragonés <sergioat7@gmail.com>
2024-03-24 20:13:19 +0100

    Load ViewModel into register compose view

--- app/src/main/java/aragones/sergio/readercollection/ui/register/RegisterScreen.kt
+++ app/src/main/java/aragones/sergio/readercollection/ui/register/RegisterScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -27,96 +28,109 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import aragones.sergio.readercollection.R
+import aragones.sergio.readercollection.ui.components.CustomCircularProgressIndicator
 import aragones.sergio.readercollection.ui.components.CustomOutlinedTextField
 import aragones.sergio.readercollection.ui.components.MainActionButton
+import com.aragones.sergio.data.auth.LoginFormState
 
 @Preview
 @Composable
 fun RegisterScreenPreview() {
-    RegisterScreen()
+//    RegisterScreen()
 }
 
 @Composable
-fun RegisterScreen() {
+fun RegisterScreen(viewModel: RegisterViewModel) {
 
+    val username by viewModel.username.observeAsState(initial = "")
+    val password by viewModel.password.observeAsState(initial = "")
+    val confirmPassword by viewModel.confirmPassword.observeAsState(initial = "")
     var passwordVisibility by rememberSaveable { mutableStateOf(false) }
+    var confirmPasswordVisibility by rememberSaveable { mutableStateOf(false) }
+    val registerFormState by viewModel.registerFormState.observeAsState(initial = LoginFormState())
+    val loading by viewModel.registerLoading.observeAsState(initial = false)
 
     val padding12 = dimensionResource(id = R.dimen.padding_12dp).value
     val padding24 = dimensionResource(id = R.dimen.padding_24dp).value
     val margin8 = dimensionResource(id = R.dimen.margin_8dp).value
     val size200 = dimensionResource(id = R.dimen.size_200dp).value
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.colorSecondary))
-            .padding(padding24.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.login_register_image),
-            contentDescription = "",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        )
-        CustomOutlinedTextField(
-            text = "username",
-            errorTextId = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = padding12.dp, end = padding12.dp, top = padding24.dp),
-            labelText = stringResource(id = R.string.username),
-            endIcon = R.drawable.ic_show_info,
-            onTextChanged = { newUsername ->
-                //TODO:
-            },
-            onEndIconClicked = {
-                //TODO:
-            }
-        )
-        CustomOutlinedTextField(
-            text = "password",
-            errorTextId = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = padding12.dp, end = padding12.dp, top = margin8.dp),
-            labelText = stringResource(id = R.string.password),
-            endIcon = if (passwordVisibility) {
-                R.drawable.ic_hide_password
-            } else {
-                R.drawable.ic_show_password
-            },
-            onTextChanged = { newPassword ->
-                //TODO:
-            },
-            onEndIconClicked = { passwordVisibility = !passwordVisibility }
-        )
-        CustomOutlinedTextField(
-            text = "confirm_password",
-            errorTextId = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = padding12.dp, end = padding12.dp, top = margin8.dp),
-            labelText = stringResource(id = R.string.confirm_password),
-            endIcon = if (passwordVisibility) {
-                R.drawable.ic_hide_password
-            } else {
-                R.drawable.ic_show_password
-            },
-            onTextChanged = { newPassword ->
-                //TODO:
-            },
-            onEndIconClicked = { passwordVisibility = !passwordVisibility }
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        MainActionButton(
-            text = stringResource(id = R.string.sign_in),
-            modifier = Modifier
-                .width(size200.dp)
-                .align(Alignment.CenterHorizontally)
-                .padding(horizontal = padding12.dp, vertical = padding24.dp),
-            enabled = true
+    if (loading) {
+        CustomCircularProgressIndicator()
+    } else {
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(colorResource(id = R.color.colorSecondary))
+                .padding(padding24.dp)
         ) {
-            //TODO:
+            Image(
+                painter = painterResource(id = R.drawable.login_register_image),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            CustomOutlinedTextField(
+                text = username,
+                errorTextId = registerFormState.usernameError,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = padding12.dp, end = padding12.dp, top = padding24.dp),
+                labelText = stringResource(id = R.string.username),
+                endIcon = R.drawable.ic_show_info,
+                onTextChanged = { newUsername ->
+                    viewModel.registerDataChanged(newUsername, password, confirmPassword)
+                },
+                onEndIconClicked = {
+                    viewModel.showInfoDialog(R.string.username_info)
+                }
+            )
+            CustomOutlinedTextField(
+                text = password,
+                errorTextId = registerFormState.passwordError,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = padding12.dp, end = padding12.dp, top = margin8.dp),
+                labelText = stringResource(id = R.string.password),
+                endIcon = if (passwordVisibility) {
+                    R.drawable.ic_hide_password
+                } else {
+                    R.drawable.ic_show_password
+                },
+                onTextChanged = { newPassword ->
+                    viewModel.registerDataChanged(username, newPassword, confirmPassword)
+                },
+                onEndIconClicked = { passwordVisibility = !passwordVisibility }
+            )
+            CustomOutlinedTextField(
+                text = confirmPassword,
+                errorTextId = registerFormState.passwordError,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = padding12.dp, end = padding12.dp, top = margin8.dp),
+                labelText = stringResource(id = R.string.confirm_password),
+                endIcon = if (confirmPasswordVisibility) {
+                    R.drawable.ic_hide_password
+                } else {
+                    R.drawable.ic_show_password
+                },
+                onTextChanged = { newPassword ->
+                    viewModel.registerDataChanged(username, password, newPassword)
+                },
+                onEndIconClicked = { confirmPasswordVisibility = !confirmPasswordVisibility }
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            MainActionButton(
+                text = stringResource(id = R.string.sign_up),
+                modifier = Modifier
+                    .width(size200.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .padding(horizontal = padding12.dp, vertical = padding24.dp),
+                enabled = registerFormState.isDataValid
+            ) {
+                viewModel.register(username, password)
+            }
         }
     }
 }
