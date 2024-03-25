ffd56b5b87340d2105618390a25149fc08fe566b
Sergio Aragonés <sergioat7@gmail.com>
2024-03-25 19:40:46 +0100

    Remove Login fragment

--- app/src/main/java/aragones/sergio/readercollection/ui/base/BaseActivity.kt
+++ app/src/main/java/aragones/sergio/readercollection/ui/base/BaseActivity.kt
@@ -5,6 +5,17 @@
 
 package aragones.sergio.readercollection.ui.base
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
-open class BaseActivity: AppCompatActivity()
+open class BaseActivity : AppCompatActivity() {
+
+    protected fun <T> launchActivity(activity: Class<T>, clearStack: Boolean = false) {
+
+        val intent = Intent(this, activity)
+        if (clearStack) {
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        startActivity(intent)
+    }
+}
