4ec23282dafbc5a5bee4de99fcbe8e3650c84781
Sergio Aragonés <sergioat7@gmail.com>
2024-03-25 19:26:52 +0100

    Code refactoring

--- app/src/main/java/aragones/sergio/readercollection/ui/components/TextFields.kt
+++ app/src/main/java/aragones/sergio/readercollection/ui/components/TextFields.kt
@@ -18,10 +18,6 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -172,8 +168,7 @@ fun CustomOutlinedTextField(
                 color = if (errorTextId != null) Color.Red else textColor,
                 fontSize = fontSize.sp,
                 fontWeight = fontWeight,
-                fontFamily = robotoSerifFamily,
-//            letterSpacing = dimensionResource(id = R.dimen.margin_5dp).value.sp
+                fontFamily = robotoSerifFamily
             ),
             label = label,
             placeholder = placeholder,
