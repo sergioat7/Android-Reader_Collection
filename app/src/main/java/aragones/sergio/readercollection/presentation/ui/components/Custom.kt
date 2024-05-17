c113c636d8f1e18a9d56adb82e0e46b0788237b1
Sergio Aragonés <sergioat7@gmail.com>
2024-05-17 18:58:13 +0200

    Create no results component

--- app/src/main/java/aragones/sergio/readercollection/presentation/ui/components/Custom.kt
+++ app/src/main/java/aragones/sergio/readercollection/presentation/ui/components/Custom.kt
@@ -5,22 +5,67 @@
 
 package aragones.sergio.readercollection.presentation.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import aragones.sergio.readercollection.R
+
+@Preview(showBackground = true)
+@Composable
+fun NoResultsComponent(text: String = stringResource(id = R.string.no_results_text)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image_no_results),
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth(0.5f)
+        )
+        Text(
+            text = text,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(24.dp),
+            style = TextStyle(
+                color = colorResource(id = R.color.textPrimary),
+                fontFamily = robotoSerifFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = dimensionResource(id = R.dimen.text_size_20sp).value.sp,
+                textAlign = TextAlign.Center,
+            )
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
