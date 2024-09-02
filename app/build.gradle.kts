f672a5e46c4325194801a8c128262654baa25469
Sergio Aragonés <sergioat7@gmail.com>
2024-09-02 09:50:22 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ val appName = "aragones.sergio.readercollection"
 
 val versionMajor = 2
 val versionMinor = 5
-val versionPatch = 4
+val versionPatch = 5
 val versionBuild = 0 // bump for dogfood builds, public betas, etc.
 
 android {
