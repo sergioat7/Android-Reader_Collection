b57b1c1e5ded35a1920ea44f78b9714b7bc4f576
Sergio Aragonés <sergioat7@gmail.com>
2025-01-14 21:01:40 +0100

    Delete fragments and unused code

--- app/src/main/java/aragones/sergio/readercollection/presentation/ui/base/BaseViewModel.kt
+++ app/src/main/java/aragones/sergio/readercollection/presentation/ui/base/BaseViewModel.kt
@@ -5,15 +5,7 @@
 
 package aragones.sergio.readercollection.presentation.ui.base
 
-import android.content.Context
-import android.view.Gravity
-import android.widget.LinearLayout
-import android.widget.NumberPicker
 import androidx.lifecycle.ViewModel
-import aragones.sergio.readercollection.R
-import aragones.sergio.readercollection.presentation.extensions.getPickerParams
-import aragones.sergio.readercollection.presentation.extensions.setup
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 open class BaseViewModel : ViewModel() {
@@ -24,51 +16,5 @@ open class BaseViewModel : ViewModel() {
         super.onCleared()
 
         disposables.clear()
-    }
-
-    fun sort(
-        context: Context,
-        sortParam: String?,
-        isSortDescending: Boolean,
-        acceptHandler: ((newSortParam: String?, newIsSortDescending: Boolean) -> Unit)?,
-    ) {
-        val sortingKeys = context.resources.getStringArray(R.array.sorting_param_keys)
-        val sortingValues = context.resources.getStringArray(R.array.sorting_param_values)
-
-        val dialogView = LinearLayout(context)
-        dialogView.orientation = LinearLayout.HORIZONTAL
-
-        val sortKeysPicker = NumberPicker(context)
-        sortKeysPicker.setup(sortingValues)
-        sortParam?.let {
-            sortKeysPicker.value = sortingKeys.indexOf(it)
-        }
-
-        val sortOrdersPicker = NumberPicker(context)
-        sortOrdersPicker.setup(context.resources.getStringArray(R.array.sorting_order_values))
-        sortOrdersPicker.value = if (isSortDescending) 1 else 0
-
-        val params = LinearLayout.LayoutParams(50, 50)
-        params.gravity = Gravity.CENTER
-
-        dialogView.layoutParams = params
-        dialogView.addView(sortKeysPicker, getPickerParams())
-        dialogView.addView(sortOrdersPicker, getPickerParams())
-
-        MaterialAlertDialogBuilder(context)
-            .setTitle(context.resources.getString(R.string.order_by))
-            .setView(dialogView)
-            .setCancelable(false)
-            .setPositiveButton(context.resources.getString(R.string.accept)) { dialog, _ ->
-
-                val sortValue = sortingKeys[sortKeysPicker.value]
-                acceptHandler?.invoke(
-                    sortValue.ifBlank { null },
-                    sortOrdersPicker.value == 1,
-                )
-                dialog.dismiss()
-            }.setNegativeButton(context.resources.getString(R.string.cancel)) { dialog, _ ->
-                dialog.dismiss()
-            }.show()
     }
 }
