f92d6ae5dd9c87a7cacee0f11dfc6d219cced837
Sergio Aragonés <sergioat7@gmail.com>
2025-07-05 19:21:02 +0200

    Increase line spacing in dialogs text

--- app/src/main/java/aragones/sergio/readercollection/presentation/components/Dialogs.kt
+++ app/src/main/java/aragones/sergio/readercollection/presentation/components/Dialogs.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import aragones.sergio.readercollection.R
@@ -412,6 +413,7 @@ private fun TextMessageAlertDialog(text: String, modifier: Modifier = Modifier) 
         modifier = modifier,
         style = MaterialTheme.typography.bodyLarge,
         color = MaterialTheme.colorScheme.primary,
+        lineHeight = 24.sp,
     )
 }
 
