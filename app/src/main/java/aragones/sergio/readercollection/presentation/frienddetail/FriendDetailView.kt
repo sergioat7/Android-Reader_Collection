c55bf408518b2c01633f21b13227b08c9e753b18
Sergio Aragonés <sergioat7@gmail.com>
2025-07-17 17:29:32 +0200

    Show friend data

--- app/src/main/java/aragones/sergio/readercollection/presentation/frienddetail/FriendDetailView.kt
+++ app/src/main/java/aragones/sergio/readercollection/presentation/frienddetail/FriendDetailView.kt
@@ -9,12 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import aragones.sergio.readercollection.presentation.components.InformationAlertDialog
 import aragones.sergio.readercollection.presentation.theme.ReaderCollectionApp
 
 @Composable
 fun FriendDetailView(onBack: () -> Unit, viewModel: FriendDetailViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
+    val error by viewModel.error.collectAsState()
 
     ReaderCollectionApp {
         FriendDetailScreen(
@@ -22,6 +25,22 @@ fun FriendDetailView(onBack: () -> Unit, viewModel: FriendDetailViewModel = hilt
             onBack = onBack,
             onDeleteFriend = {},
         )
+    }
+
+    val text = if (error != null) {
+        val errorText = StringBuilder()
+        if (requireNotNull(error).error.isNotEmpty()) {
+            errorText.append(requireNotNull(error).error)
+        } else {
+            errorText.append(stringResource(requireNotNull(error).errorKey))
+        }
+        errorText.toString()
+    } else {
+        ""
+    }
+    InformationAlertDialog(show = text.isNotEmpty(), text = text) {
+        viewModel.closeDialogs()
+        onBack()
     }
 
     LaunchedEffect(Unit) {
--- app/src/main/java/aragones/sergio/readercollection/presentation/frienddetail/FriendDetailViewModel.kt
+++ app/src/main/java/aragones/sergio/readercollection/presentation/frienddetail/FriendDetailViewModel.kt
@@ -7,9 +7,15 @@ package aragones.sergio.readercollection.presentation.frienddetail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.toRoute
+import aragones.sergio.readercollection.R
+import aragones.sergio.readercollection.data.remote.model.ErrorResponse
+import aragones.sergio.readercollection.domain.UserRepository
 import aragones.sergio.readercollection.presentation.base.BaseViewModel
 import aragones.sergio.readercollection.presentation.navigation.Route
+import com.aragones.sergio.util.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,20 +23,58 @@ import kotlinx.coroutines.flow.StateFlow
 @HiltViewModel
 class FriendDetailViewModel @Inject constructor(
     state: SavedStateHandle,
+    private val userRepository: UserRepository,
 ) : BaseViewModel() {
 
     //region Private properties
     private val params = state.toRoute<Route.FriendDetail>()
     private var _state: MutableStateFlow<FriendDetailUiState> =
         MutableStateFlow(FriendDetailUiState.Loading)
+    private val _error = MutableStateFlow<ErrorResponse?>(null)
     //endregion
 
     //region Public properties
     val state: StateFlow<FriendDetailUiState> = _state
+    val error: StateFlow<ErrorResponse?> = _error
+    //endregion
+
+    //region Lifecycle methods
+    override fun onCleared() {
+        super.onCleared()
+
+        userRepository.onDestroy()
+    }
     //endregion
 
     //region Public methods
     fun fetchFriend() {
+        userRepository
+            .getFriend(params.userId)
+            .subscribeBy(
+                onSuccess = {
+                    _state.value = FriendDetailUiState.Success(it)
+                },
+                onError = {
+                    when (it) {
+                        is NoSuchElementException -> {
+                            _error.value = ErrorResponse(
+                                Constants.EMPTY_VALUE,
+                                R.string.no_friends_found,
+                            )
+                        }
+                        else -> {
+                            _error.value = ErrorResponse(
+                                Constants.EMPTY_VALUE,
+                                R.string.error_server,
+                            )
+                        }
+                    }
+                },
+            ).addTo(disposables)
+    }
+
+    fun closeDialogs() {
+        _error.value = null
     }
     //endregion
 }
