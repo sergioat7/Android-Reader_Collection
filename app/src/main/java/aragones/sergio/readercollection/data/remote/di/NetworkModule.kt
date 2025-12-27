615aa8ea3e5c98e6e8c3545153b2539961877540
Sergio Aragonés <sergioat7@gmail.com>
2025-12-27 21:31:14 +0100

    Create ktor client

--- app/src/main/java/aragones/sergio/readercollection/data/remote/di/NetworkModule.kt
+++ app/src/main/java/aragones/sergio/readercollection/data/remote/di/NetworkModule.kt
@@ -5,6 +5,7 @@
 
 package aragones.sergio.readercollection.data.remote.di
 
+import aragones.sergio.readercollection.BuildConfig
 import aragones.sergio.readercollection.data.remote.ApiManager
 import aragones.sergio.readercollection.data.remote.services.GoogleApiService
 import com.google.firebase.Firebase
@@ -19,17 +20,73 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_GOOGLE_ENDPOINT = "https://www.googleapis.com/books/v1/"
+    private const val BASE_GOOGLE_ENDPOINT = "www.googleapis.com/books/v1"
+    private const val API_KEY = "key"
 
     @Singleton
     @Provides
-    fun providesGoogleApiService(): GoogleApiService = ApiManager.getService(BASE_GOOGLE_ENDPOINT)
+    fun providesGoogleApiService(): GoogleApiService =
+        ApiManager.getService("https://www.googleapis.com/books/v1/")
+
+    @Provides
+    fun providesHttpClientEngine(): HttpClientEngine = OkHttp.create {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.HEADERS
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+        preconfigured = OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(2, TimeUnit.MINUTES)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .followRedirects(false)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesHttpClient(httpClientEngine: HttpClientEngine): HttpClient =
+        HttpClient(httpClientEngine) {
+            engine {
+                httpClientEngine.config
+            }
+        }.config {
+            install(ContentNegotiation) {
+                json(
+                    json = Json { ignoreUnknownKeys = true },
+                    contentType = ContentType.Application.Json,
+                )
+            }
+            install(DefaultRequest) {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = BASE_GOOGLE_ENDPOINT
+                    parameters.append(API_KEY, BuildConfig.API_KEY)
+                }
+            }
+        }
 
     @Singleton
     @Provides
