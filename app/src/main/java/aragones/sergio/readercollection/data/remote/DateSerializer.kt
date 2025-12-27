b17f7e9c9d5ae94e1b3f7385eac1e5ba317c6e68
Sergio Aragonés <sergioat7@gmail.com>
2025-12-27 21:33:48 +0100

    Create date serializer

---
+++ app/src/main/java/aragones/sergio/readercollection/data/remote/DateSerializer.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 27/12/2025
+ */
+
+package aragones.sergio.readercollection.data.remote
+
+import com.aragones.sergio.util.extensions.toDate
+import com.aragones.sergio.util.extensions.toString
+import java.util.Date
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+object DateSerializer : KSerializer<Date?> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun serialize(encoder: Encoder, value: Date?) {
+        value.toString()?.let { dateString ->
+            encoder.encodeString(dateString)
+        } ?: encoder.encodeNull()
+    }
+
+    override fun deserialize(decoder: Decoder): Date? = decoder.decodeString().toDate()
+}
