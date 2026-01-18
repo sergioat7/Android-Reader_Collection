9d7f75a1bded0ce50c6fd0e2f6947be26378240f
Sergio Aragonés <sergioat7@gmail.com>
2026-01-18 19:32:05 +0100

    Create ios app

---
+++ iosApp/ReaderCollection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReaderCollection
+//
+//  Created by Sergio Aragonés on 18/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosApp/ReaderCollection/ReaderCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderCollectionApp.swift
+//  ReaderCollection
+//
+//  Created by Sergio Aragonés on 18/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ReaderCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
