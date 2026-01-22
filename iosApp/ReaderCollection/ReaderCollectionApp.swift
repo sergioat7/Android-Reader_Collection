7a37aa6bfb84da1824621a4d2edbb7ac05313870
Sergio Aragonés <sergioat7@gmail.com>
2026-01-22 20:33:12 +0100

    Add Firebase to iOS project

--- iosApp/ReaderCollection/ReaderCollectionApp.swift
+++ iosApp/ReaderCollection/ReaderCollectionApp.swift
@@ -5,10 +5,16 @@
 //  Created by Sergio Aragonés on 18/1/26.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct ReaderCollectionApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
