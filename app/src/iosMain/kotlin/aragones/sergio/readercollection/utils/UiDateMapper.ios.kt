4e146aa2248665d0cdc71614f438cb48e300e117
Sergio Aragonés <sergioat7@gmail.com>
2026-01-24 13:18:59 +0100

    Implement date mapper

--- app/src/iosMain/kotlin/aragones/sergio/readercollection/utils/UiDateMapper.ios.kt
+++ app/src/iosMain/kotlin/aragones/sergio/readercollection/utils/UiDateMapper.ios.kt
@@ -5,7 +5,19 @@
 
 package aragones.sergio.readercollection.utils
 
+import com.aragones.sergio.util.Constants
+import com.aragones.sergio.util.extensions.toLocalDate
+import com.aragones.sergio.util.extensions.toString
 import kotlinx.datetime.LocalDate
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
+import platform.Foundation.NSTimeZone
+import platform.Foundation.dateWithTimeIntervalSince1970
+import platform.Foundation.localTimeZone
+import platform.Foundation.preferredLanguages
+import platform.Foundation.timeIntervalSince1970
+import platform.Foundation.timeZoneWithAbbreviation
 
 actual object UiDateMapper {
 
@@ -13,22 +25,90 @@ actual object UiDateMapper {
         pattern: String,
         language: String,
     ): Map<String, List<Any>> {
-        TODO("Not yet implemented")
+        val locale = NSLocale(language)
+        val simpleDateFormat = NSDateFormatter().apply {
+            this.dateFormat = pattern
+            this.locale = locale
+        }
+        return this.mapNotNull { it.toString(format = null).toDate() }.groupBy {
+            simpleDateFormat.stringFromDate(it)
+        }
     }
 
-    actual fun LocalDate?.getValueToShow(language: String): String? {
-        TODO("Not yet implemented")
-    }
+    actual fun LocalDate?.getValueToShow(language: String): String? = this
+        ?.toString(format = null)
+        .toDate()
+        .toString(
+            format = Constants.getDateFormatToShow(language),
+            language = language,
+        )
 
     actual fun String.toLong(language: String?): Long? {
-        TODO("Not yet implemented")
+        val date = this
+            .toDate(
+                format = language?.let { Constants.getDateFormatToShow(it) },
+                language = language,
+                timeZone = NSTimeZone.timeZoneWithAbbreviation("UTC"),
+            )
+        return date?.let {
+            (it.timeIntervalSince1970 * 1000).toLong()
+        }
     }
 
-    actual fun Long.toLocalDate(language: String): LocalDate? {
-        TODO("Not yet implemented")
-    }
+    actual fun Long.toLocalDate(language: String): LocalDate? = NSDate
+        .dateWithTimeIntervalSince1970(this / 1000.0)
+        .toString(format = null)
+        .toLocalDate()
 
     actual fun Int.toMonthName(language: String): String {
-        TODO("Not yet implemented")
+        val formatter = NSDateFormatter()
+        formatter.setLocale(NSLocale(language))
+        return formatter.monthSymbols[this - 1].toString().replaceFirstChar { it.uppercase() } + ","
+    }
+
+    private fun String?.toDate(
+        format: String? = null,
+        language: String? = null,
+        timeZone: NSTimeZone? = null,
+    ): NSDate? {
+        val dateFormat = format ?: Constants.DATE_FORMAT
+        val locale = language ?: run {
+            (NSLocale.preferredLanguages.first() as String).split("-").first()
+        }
+
+        val simpleDateFormat = NSDateFormatter().apply {
+            this.dateFormat = dateFormat
+            this.locale = NSLocale(locale)
+            this.timeZone = timeZone ?: NSTimeZone.localTimeZone
+        }
+
+        return this?.let {
+            try {
+                simpleDateFormat.dateFromString(it)
+            } catch (_: Exception) {
+                null
+            }
+        }
+    }
+
+    private fun NSDate?.toString(format: String? = null, language: String? = null): String? {
+        val dateFormat = format ?: Constants.DATE_FORMAT
+        val locale = language ?: run {
+            (NSLocale.preferredLanguages.first() as String).split("-").first()
+        }
+
+        val simpleDateFormat = NSDateFormatter().apply {
+            this.dateFormat = dateFormat
+            this.locale = NSLocale(locale)
+            this.timeZone = NSTimeZone.localTimeZone
+        }
+
+        return this?.let {
+            try {
+                simpleDateFormat.stringFromDate(it)
+            } catch (_: Exception) {
+                null
+            }
+        }
     }
 }
