0eec8b4125ef97d0b2921b7cc5d315b55665aac2
Sergio Aragonés <sergioat7@gmail.com>
2026-01-25 12:43:25 +0100

    Create Statistics screen for iOS

--- app/src/iosMain/kotlin/aragones/sergio/readercollection/presentation/statistics/StatisticsView.ios.kt
+++ app/src/iosMain/kotlin/aragones/sergio/readercollection/presentation/statistics/StatisticsView.ios.kt
@@ -6,6 +6,10 @@
 package aragones.sergio.readercollection.presentation.statistics
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import aragones.sergio.readercollection.presentation.components.LaunchedEffectOnce
+import aragones.sergio.readercollection.presentation.theme.ReaderCollectionApp
 
 @Composable
 actual fun StatisticsView(
@@ -13,4 +17,28 @@ actual fun StatisticsView(
     onShowAll: (String?, Boolean, Int, Int, String?, String?) -> Unit,
     viewModel: StatisticsViewModel,
 ) {
+    val state by viewModel.state.collectAsState()
+
+    ReaderCollectionApp(navigationBarSameAsBackground = false) {
+        StatisticsScreen(
+            state = state,
+            onImportClick = {},
+            onExportClick = {},
+            onGroupClick = { year, month, author, format ->
+                onShowAll(
+                    viewModel.sortParam,
+                    viewModel.isSortDescending,
+                    year ?: -1,
+                    month ?: -1,
+                    author,
+                    format,
+                )
+            },
+            onBookClick = onBookClick,
+        )
+    }
+
+    LaunchedEffectOnce {
+        viewModel.fetchBooks()
+    }
 }
